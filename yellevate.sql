b53baa228772b5e04756068104c3a3cc5241a541
Timothy James Franco <36463027+timfranco@users.noreply.github.com>
2022-12-09 20:37:43 +0800

    Add files via upload

---
+++ yellevate.sql
@@ -0,0 +1,13 @@
+CREATE TABLE yellevate (
+country varchar, 
+customer_id varchar,
+invoice_number numeric, 
+invoice_date date,
+due_date date,
+invoice_amount numeric,
+disputed numeric,
+dispute_lost numeric,
+settled_date date,
+days_settled integer,
+days_late integer
+);
